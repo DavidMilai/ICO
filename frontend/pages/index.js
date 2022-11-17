0671dc53de5e18385a67fe2e41235812586cfbab
David Milai <davidmilai67@gmail.com>
2022-11-17 12:16:08 +0300

    connecting wallet

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,8 +1,56 @@
 import Head from "next/head";
 import Image from "next/image";
+import { useEffect, useRef, useState } from "react";
 import styles from "../styles/Home.module.css";
+import Web3Modal from "web3modal";
+import { BigNumber, Contract, providers, utils } from "ethers";
 
 export default function Home() {
+  const [walletConnected, setWalletConnected] = useState(false);
+  const web3ModalRef = useRef();
+
+  const getProviderOrSigner = async (needSigner = false) => {
+    try {
+      const provider = await web3ModalRef.current.connect();
+      const web3Provider = new providers.Web3Provider(provider);
+
+      const { chainId } = await web3Provider.getNetwork();
+
+      if (chainId !== 5) {
+        window.alert("Change the network to Goerli");
+        throw new Error("Change network to Goerli");
+      }
+  
+      if (needSigner) {
+        const signer = web3Provider.getSigner();
+        return signer;
+      }
+      return web3Provider;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    if (!walletConnected) {
+      web3ModalRef.current = new Web3Modal({
+        network: "goerli",
+        providerOptions: {},
+        disableInjectedProvider: false,
+      });
+      ConnectToWalletConnect();
+    }
+  }, []);
+
+  const ConnectToWalletConnect = async () => {
+    try {
+      await getProviderOrSigner();
+      setWalletConnected(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div>
       <Head>
