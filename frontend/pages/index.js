101fbf768024a1daaf8b695d1d54c90da148eb70
David Milai <davidmilai67@gmail.com>
2022-11-17 11:21:05 +0300

    body

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -10,6 +10,14 @@ export default function Home() {
         <meta name="description" content="ICO-dAPP" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <div className={styles.main}>
+        <div>
+          <h1 className={styles.title}>Welcome to Crypto Devs ICO</h1>
+          <div className={styles.description}>
+            You can claim or mint Crypto tokens here
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
